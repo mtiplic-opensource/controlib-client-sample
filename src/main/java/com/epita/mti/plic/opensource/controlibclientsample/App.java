54d2c7a3eb4b8c3a1bdfeb002bb6c3ae8da1561d
Julien Fraisse <roulyo.42@gmail.com>
2012-04-04 12:24:35 +0200

    Client demonstration.

--- src/main/java/com/epita/mti/plic/opensource/controlibclientsample/App.java
+++ src/main/java/com/epita/mti/plic/opensource/controlibclientsample/App.java
@@ -1,13 +1,31 @@
 package com.epita.mti.plic.opensource.controlibclientsample;
 
+import com.epita.mti.plic.opensource.controlibutility.beans.CLPressure;
+import com.epita.mti.plic.opensource.controlibutility.serialization.ObjectSender;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+
+  public static void main(String[] args) throws Exception
+  {
+    Socket socket = new Socket("127.0.0.1", 4200);
+    ObjectSender sender = new ObjectSender(socket.getOutputStream());
+
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    String s;
+    while ((s = in.readLine()) != null && s.length() != 0)
     {
-        System.out.println( "Hello World!" );
+      CLPressure data = new CLPressure();
+      
+      data.setX(Integer.parseInt(s));
+      sender.send(data);
     }
+  }
 }
